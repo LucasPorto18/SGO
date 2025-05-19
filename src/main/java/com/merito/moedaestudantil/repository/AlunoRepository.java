f080de99be04f7fd04026fcd106a30949547ed3f
LucasPorto18 <124475594+LucasPorto18@users.noreply.github.com>
2025-05-19 11:23:07 -0300

    Delete src directory

--- src/main/java/com/merito/moedaestudantil/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.merito.moedaestudantil.repository;
-import com.merito.moedaestudantil.model.Aluno;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Optional<Aluno> findByCpfAndSenha(String cpf, String senha);
-}
--- src/main/java/com/merito/moedaestudantil/repository/InstituicaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.merito.moedaestudantil.repository;
-
-import com.merito.moedaestudantil.model.Instituicao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstituicaoRepository extends JpaRepository<Instituicao, Long> {
-}
